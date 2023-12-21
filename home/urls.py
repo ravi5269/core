2a0ebf2e97a80c0867f35658a5f79fd594bd94d5
ravi <ravipatelkant@gmail.com>
2023-12-21 21:07:18 +0530

    funcation based view created

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,19 @@
+
+
+
 from django.urls import path
-from home.views import home, post_student
+from home.views import home,post_student,upadte_student,partial_upadte_student,delete_student
 from django.contrib import admin
 
-urlpatterns = [path("get_student/", home), path("post_student/", post_student)]
+urlpatterns = [
+   
+    path('get_student/',home),
+    path('post_student/',post_student),
+    path('update_student/',upadte_student),
+    path('update_student/<int:pk>',upadte_student),
+    path('partial_update_student/<int:pk>',partial_upadte_student),
+    path('delete_student/<int:pk>',delete_student)
+    
+
+    
+]
--- home/views.py
+++ home/views.py
@@ -8,24 +8,66 @@ from home.models import Student
 from home.serializers import StudentSerializer
 
 
-@api_view(["GET"])
+@api_view(['GET'])
 def home(request):
     student_objs = Student.objects.all()
-    serializer = StudentSerializer(student_objs, many=True)
+    serializer = StudentSerializer(student_objs, many =True)
 
-    return Response({"message": 200, "payload": serializer.data})
+    return Response({"message": 200, 'payload':serializer.data})
 
-
-@api_view(["POST"])
+@api_view(['POST'])
 def post_student(request):
     data = request.data
     seroalizer = StudentSerializer(data=request.data)
 
     if not seroalizer.is_valid():
         print(seroalizer.errors)
-        return Response(
-            {"message": "something went worng", "errors": seroalizer.errors}
-        )
-
+        return Response({'message':'something went worng','errors': seroalizer.errors})
+    
     seroalizer.save()
-    return Response({"messages": "success", "payload": seroalizer.data})
+    return Response({'messages':'success','payload':seroalizer.data})
+
+
+@api_view(['PUT'])
+def upadte_student(request,pk):
+    try:
+        student_obj = Student.objects.get(pk = pk)
+        seroalizer = StudentSerializer(student_obj,data = request.data)
+
+        if not seroalizer.is_valid():
+            print(seroalizer.errors)
+            return Response({'message':'something went worng','errors': seroalizer.errors})
+        
+        seroalizer.save()
+        return Response({'messages':'success','payload':seroalizer.data})
+    except Exception as e:
+        print(e)
+        return Response({"message":"invallid ID"})
+
+@api_view(['PATCH'])
+def partial_upadte_student(request,pk):
+    try:
+        student_obj = Student.objects.get(pk = pk)
+        seroalizer = StudentSerializer(student_obj,data = request.data,partial = True)
+
+        if not seroalizer.is_valid():
+            print(seroalizer.errors)
+            return Response({'message':'something went worng','errors': seroalizer.errors})
+        
+        seroalizer.save()
+        return Response({'messages':'success','payload':seroalizer.data})
+    except Exception as e:
+        print(e)
+        return Response({"message":"invallid ID"})
+    
+@api_view(['DELETE'])
+def delete_student(request,pk):
+    try:
+        student_obj = Student.objects.get(pk = pk)
+        student_obj.delete()
+        return Response({"message":"deleted success"})
+    except Exception as e:
+        print(e)
+        return Response({"message":"invallid ID"})
+
+
